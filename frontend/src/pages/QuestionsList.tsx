1cc9e645ca52c0e3df9af0664fced876e893a48c
James Vickers <jamesvickers19@yahoo.com>
2024-11-23 16:31:41 -0700

    feat: add search bar

--- frontend/src/pages/QuestionsList.tsx
+++ frontend/src/pages/QuestionsList.tsx
@@ -14,14 +14,35 @@ async function fetchQuestions() {
   return json.questions as Question[];
 }
 
+async function searchQuestions(query: string) {
+  const response = await fetch(`${serverBaseUrl}/search`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ query }),
+  });
+  if (!response.ok) {
+    console.error(`Error status ${response.status}, body: ${response.body}`);
+    return [];
+  }
+
+  const json = await response.json();
+  return json.results as Question[];
+}
+
 function QuestionsList() {
+  const [searchQuery, setSearchQuery] = useState<string | null>(null);
   const [questions, setQuestions] = useState<Question[]>([]);
   useEffect(() => {
     const loadQuestions = async () => {
-      setQuestions((await fetchQuestions()) ?? []);
+      const questions = searchQuery?.trim()?.length
+        ? await searchQuestions(searchQuery)
+        : await fetchQuestions();
+      setQuestions(questions ?? []);
     };
     loadQuestions();
-  }, []);
+  }, [searchQuery]);
   const [openedQuestion, setOpenedQuestion] = useState<Question | null>(null);
 
   return (
@@ -32,31 +53,39 @@ function QuestionsList() {
           onClose={() => setOpenedQuestion(null)}
         />
       ) : (
-        <ul>
-          {questions.map((q) => {
-            return (
-              <li
-                key={q.id}
-                style={{
-                  listStyleType: "none",
-                  padding: 0,
-                  marginBottom: "20px",
-                }}
-              >
-                <span
+        <div>
+          <label htmlFor="searchInput">Search:</label>
+          <input
+            id="searchInput"
+            type="text"
+            onChange={(e: any) => setSearchQuery(e.target.value)}
+          ></input>
+          <ul>
+            {questions.map((q) => {
+              return (
+                <li
+                  key={q.id}
                   style={{
-                    color: "blue",
-                    textDecoration: "underline",
-                    cursor: "pointer",
+                    listStyleType: "none",
+                    padding: 0,
+                    marginBottom: "20px",
                   }}
-                  onClick={() => setOpenedQuestion(q)}
                 >
-                  {`${q.title} (created ${q.created_at})`}
-                </span>
-              </li>
-            );
-          })}
-        </ul>
+                  <span
+                    style={{
+                      color: "blue",
+                      textDecoration: "underline",
+                      cursor: "pointer",
+                    }}
+                    onClick={() => setOpenedQuestion(q)}
+                  >
+                    {`${q.title} (created ${q.created_at})`}
+                  </span>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
       )}
     </div>
   );
